526c25896e42eda1c569b87ea3d77d963dcc60f6
C. Zhang <chuwzhang@gmail.com>
2022-05-22 15:38:55 +0800

    fix ablation erro

--- ablation_analysis_abip.py
+++ ablation_analysis_abip.py
@@ -24,19 +24,25 @@ def cal_dev(row, k):
     except:
         return np.nan
 
-
+df[['iteration_num', 'sol_time']] = df[['iteration_num', 'sol_time']].apply(
+  pd.to_numeric, errors='coerce'
+)
+df['success'] = df['sol_status'].apply(lambda x: x.lower()).apply(lambda x: 'solved' in x or 'optimal' in x)
 df['more_iter'] = pd.to_numeric(df.apply(lambda row: cal_dev(row, 'iteration_num'), axis=1), errors='coerce')
 df['more_time'] = pd.to_numeric(df.apply(lambda row: cal_dev(row, 'sol_time'), axis=1), errors='coerce')
 df['std_time'] = df.apply(lambda row: df_std['sol_time'].get(row.name[0]), axis=1)
 df['std_iter'] = df.apply(lambda row: df_std['iteration_num'].get(row.name[0]), axis=1)
-df['more_iter_perc'] = pd.to_numeric(df['more_iter']/df['iteration_num'], errors='coerce')
-df['more_time_perc'] = pd.to_numeric(df['more_time']/df['sol_time'], errors='coerce')
+df['more_iter_perc'] = pd.to_numeric(df['more_iter'].divide(df['std_iter']), errors='coerce')
+df['more_time_perc'] = pd.to_numeric(df['more_time'].divide(df['std_time']), errors='coerce')
 dfa = df.groupby(level=1).aggregate(
     {
         "more_iter": 'mean',
         "more_iter_perc": 'mean',
         "more_time": 'mean',
-        "more_time_perc": 'mean'
+        "more_time_perc": 'mean',
+				"iteration_num": 'mean',
+				"sol_time": 'mean',
+				"success": 'sum'
     }
 )
 
